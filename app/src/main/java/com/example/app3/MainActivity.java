bce6cf27f5b0881e49b3fd03a38287e919fedb67
gkntnm <gkntnm@gmail.com>
2019-01-24 17:05:36 +0400

    Go Home and continue developing.

--- app/src/main/java/com/example/app3/MainActivity.java
+++ app/src/main/java/com/example/app3/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.app3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int GALLERY_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.buttonOpenAndConvert);
+
+        //TODO: ПОДКЛЮЧАЙ МОКСИ!
+
+        button.setOnClickListener( new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
